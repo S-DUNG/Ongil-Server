4f50dc5ba811d126b89a6e40a39d7da2127d5ea5
1ub7 <a26788271@gmail.com>
2026-09-06 23:18:07 +0900

    fix: @EnableAsync 설정 추가

--- src/main/java/sdung/ongil/OngilApplication.java
+++ src/main/java/sdung/ongil/OngilApplication.java
@@ -2,7 +2,9 @@ package sdung.ongil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class OngilApplication {
 
