cfa36caf9cfd299bdbd03e733a9e23d1ca50b6ec
1ub7 <a26788271@gmail.com>
2026-09-14 20:49:32 +0900

    fix: 공공데이터포털 빈 응답 파싱 오류로 인한 500 에러 수정

--- src/main/java/sdung/ongil/domain/environment/service/RestTemplateConfig.java
+++ src/main/java/sdung/ongil/domain/environment/service/RestTemplateConfig.java
@@ -1,13 +1,22 @@
 package sdung.ongil.domain.environment.service;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@SuppressWarnings("removal")
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(objectMapper);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(0, converter);
+        return restTemplate;
     }
 }
